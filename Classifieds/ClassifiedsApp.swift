203b3cc55b3ffb2c0a3f58bf7ff18fe29d67671f
Ahmed Elzohry <aelzohry2010@gmail.com>
2022-11-22 17:28:03 +0200

    swiftui app template

---
+++ Classifieds/ClassifiedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassifiedsApp.swift
+//  Classifieds
+//
+//  Created by Ahmed Elzohry on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClassifiedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
